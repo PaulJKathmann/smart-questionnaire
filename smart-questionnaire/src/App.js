48ba03a3e70d9114195c9ab9de364e07c92d976c
PaulJKathmann <paul.kathmann.98@gmail.com>
2023-06-11 13:28:02 +0200

    added fallback loading grid 3x3

--- smart-questionnaire/src/App.js
+++ smart-questionnaire/src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Questionnaire from './Questionnaire';
+//import Questionnaire from './Questionnaire';
 import Treatments from './Treatments';
+import FallbackLoading from './FallbackLoading';
+const Questionnaire = React.lazy(() => import('./Questionnaire'));
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/treatments" element={<Treatments/>} />
-          <Route path="/" element={<Questionnaire/>} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={
+              <Suspense fallback={<FallbackLoading />}>
+                <Questionnaire/>
+              </Suspense>
+            }/>
+            <Route path="/treatments" element={<Treatments/>} />
+          </Routes>
       </Router>
     </div>
   );
